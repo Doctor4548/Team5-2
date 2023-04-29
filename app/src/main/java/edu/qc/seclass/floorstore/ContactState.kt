311535a75355a133114722ff67e5c082691d2743
[Tianxiang Yang] <[Tianxiang.Yang99@qmail.cuny.edu]>
2023-04-29 14:21:21 -0400

    Draft#1

---
+++ app/src/main/java/edu/qc/seclass/floorstore/ContactState.kt
@@ -0,0 +1,10 @@
+package edu.qc.seclass.floorstore
+
+data class ContactState(
+    val contacts: List<Contact> = emptyList(),
+    val firstName: String = "",
+    val lastName: String = "",
+    val phoneNumber: String = "",
+    val isAddingContact: Boolean = false,
+    val sortType: SortType = SortType.TYPE
+)
---
+++ app/src/main/java/edu/qc/seclass/floorstore/SortType.kt
@@ -0,0 +1,7 @@
+package edu.qc.seclass.floorstore
+
+enum class SortType {
+    TYPE,
+    Category,
+    Species
+}
